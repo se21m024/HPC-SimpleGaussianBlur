131b512cbfb5050a0bbb3a4dea5020a341b489bc
Thomas Stummer <thomas.stummer@grassfish.com>
2022-05-12 12:38:35 +0200

    Cleaned solution and added readme

--- SimpleGaussianBlur/Program.cs
+++ SimpleGaussianBlur/Program.cs
@@ -7,13 +7,8 @@ namespace SimpleGaussianBlur
     {
         static void Main(string[] args)
         {
-            // Demo code from lecture -> Remove this if not required any more
-            //DemoExample.DoStuff();
-
             using (var inputImage = new Bitmap("./InputImage.bmp"))
             {
-                // For testing purposes: .NET implementation without using OpenCL -> Remove this if not required any more
-                //var outputImage = GaussianNet.ApplyGaussianBlur(inputImage);
                 var outputImage = GaussianOpenCl.ApplyGaussianBlur(inputImage);
                 outputImage.Save("./OutputImage.bmp");
             }
---
+++ readme.md
@@ -0,0 +1,13 @@
+Students:<br>
+Thomas Bründl<br>
+Thomas Stummer<br><br>
+
+The source code can be found in the following files:<br>
+.\SimpleGaussianBlur\Program.cs<br>
+.\SimpleGaussianBlur\GaussianOpenCl.cs<br>
+.\SimpleGaussianBlur\blurKernel.cl<br>
+
+The compiled executable can be found here:<br>
+.\SimpleGaussianBlur\bin\Debug\SimpleGaussianBlur.exe<br><br>
+
+When exectuing this executable, the input image "InputImage.bmp" in the same directory as the .exe File is read and the blury output image "OutputImage.bmp" is created (also in the same directory).
